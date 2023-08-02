7d7b6e9c1640208180027ab2d0a4c93a9277d3a6
kumarsanjeev762 <58472067+kumarsanjeev762@users.noreply.github.com>
2023-08-02 20:36:14 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,5 @@
+function skills() {
+    var skills = ["HTML", "CSS", "JS", "PHP", "MySQL"];
+    var skillsString = skills.join(", ");
+    document.getElementById("skills").innerHTML = skillsString;
+}
